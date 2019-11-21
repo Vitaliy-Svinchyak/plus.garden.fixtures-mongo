0ba36bb25b943850b77634ebbd7cb9882595437d
e33 <vitalii.svinchyak@gmail.com>
2019-11-21 15:35:46 +0200

    Clear table, not delete

--- MongoFixtureLoader.js
+++ MongoFixtureLoader.js
@@ -1,12 +1,43 @@
 let MongoFixtureLoader = function (Garden, config, logger) {
     const Fixtures = require('node-mongodb-fixtures')
+
+    Fixtures.prototype.clear = function () {
+        const assert = require('assert')
+        const fs = require('fs')
+        const path = require('path')
+
+        const readdir = Promise.promisify(fs.readdir)
+
+        assert(this._db, 'must call connect')
+
+        return readdir(this._dir).then(files => {
+            const promises = files.filter(this._filterFiles).map(file => {
+                const parse = path.parse(file)
+                const collectionName = parse.name
+                const ext = parse.ext
+
+                if (!isSupportedExt(ext) || isScript(collectionName, ext)) {
+                    return null
+                }
+
+                const dropCollection = this._db.collection(collectionName).
+                    remove({}).
+                    catch(e => {
+                        if (e.code !== 26) throw e
+                    })
+                return dropCollection
+            })
+            return Promise.all(promises).tap(() => log(this._mute, '[done ] *unload all')).then(() => this)
+        })
+    }
+
     let paths = config.get('fixtures-mongo:fixtures')
     let dbUri = config.get('fixtures-mongo:uri')
 
     const fixtures = new Fixtures({
         dir: paths,
         filter: '.*',
-        mute: true
+        mute: true,
     })
 
     this.load = async function () {
@@ -28,7 +59,7 @@ let MongoFixtureLoader = function (Garden, config, logger) {
         logger.info('Dropping fixtures: ' + dbUri)
         try {
             await fixtures.connect(dbUri)
-            await fixtures.unload()
+            await fixtures.clear()
         } catch (e) {
             console.error(e)
         } finally {
